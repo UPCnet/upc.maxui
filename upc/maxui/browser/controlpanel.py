b7a4629187c3e50dc044bf12c72e7c7bcf5dd5eb
Víctor Fernández de Alba <victor.fernandez@upcnet.es>
2012-02-23 09:03:12 +0100

    controlpanel

--- upc/maxui/browser/controlpanel.py
+++ upc/maxui/browser/controlpanel.py
@@ -16,8 +16,10 @@ from upc.maxui import UPCMAXUIMessageFactory as _
 DEFAULT_OAUTH_TOKEN_ENDPOINT = 'https://oauth.upc.edu/token'
 DEFAULT_OAUTH_GRANT_TYPE = 'password'
 DEFAULT_MAX_SERVER = 'https://max.beta.upcnet.es'
-DEFAULT_MAX_USERNAME = 'operations'
-DEFAULT_MAX_PASSWORD = 'operations'
+DEFAULT_MAX_OPS_USERNAME = 'operations'
+DEFAULT_MAX_OPS_PASSWORD = 'operations'
+DEFAULT_MAX_APP_USERNAME = 'apppass'
+DEFAULT_MAX_APP_PASSWORD = 'apppass'
 
 
 class IMAXUISettings(Interface):
@@ -51,19 +53,35 @@ class IMAXUISettings(Interface):
         )
 
     max_ops_username = schema.ASCIILine(
-        title=_(u'label_max_username', default=u'MAX operations agent username'),
-        description=_(u'help_max_username',
+        title=_(u'label_max_ops_username', default=u'MAX operations agent username'),
+        description=_(u'help_max_ops_username',
                         default=u"Please, specify the MAX operations agent url."),
         required=True,
-        default=DEFAULT_MAX_USERNAME
+        default=DEFAULT_MAX_OPS_USERNAME
         )
 
     max_ops_password = schema.ASCIILine(
-        title=_(u'label_max_password', default=u'MAX operations agent password'),
-        description=_(u'help_max_password',
+        title=_(u'label_max_ops_password', default=u'MAX operations agent password'),
+        description=_(u'help_max_ops_password',
                         default=u"Please, specify the MAX operations agent password."),
         required=True,
-        default=DEFAULT_MAX_PASSWORD
+        default=DEFAULT_MAX_OPS_PASSWORD
+        )
+
+    max_app_username = schema.ASCIILine(
+        title=_(u'label_max_app_username', default=u'MAX application agent username'),
+        description=_(u'help_max_app_username',
+                        default=u"Please, specify the MAX application agent url."),
+        required=True,
+        default=DEFAULT_MAX_APP_USERNAME
+        )
+
+    max_app_password = schema.ASCIILine(
+        title=_(u'label_max_app_password', default=u'MAX application agent password'),
+        description=_(u'help_max_app_password',
+                        default=u"Please, specify the MAX application agent password."),
+        required=True,
+        default=DEFAULT_MAX_APP_PASSWORD
         )
 
 
@@ -101,9 +119,16 @@ class MAXUISettingsEditForm(controlpanel.RegistryEditForm):
         self.request.response.redirect("%s/%s" % (self.context.absolute_url(),
                                                   self.control_panel_view))
 
+    @button.buttonAndHandler(_(u'Get token'), name='getToken')
+    def handleGetToken(self, action, data):
+        credentials = dict(login=data.max_app_username,
+                           password=data.max_app_password)
+        from upc.maxui.max import getToken
+        oauth_token = getToken(credentials)
+
 
 class MAXUISettingsControlPanel(controlpanel.ControlPanelFormWrapper):
     """MAXUI settings control panel.
     """
     form = MAXUISettingsEditForm
-    #index = ViewPageTemplateFile('controlpanel.pt')
+    index = ViewPageTemplateFile('controlpanel.pt')
